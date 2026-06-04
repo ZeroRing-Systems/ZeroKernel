f052b423e44fd8a807eb254353b671b0dd603044
IFKabir <ifk.ishanfalak@gmail.com>
2026-06-04 11:41:20 +0530

    add vfs syscalls and storage mode to hal

--- interfaces/hal.h
+++ interfaces/hal.h
@@ -10,4 +10,9 @@ public:
     virtual int read_key() = 0;
     virtual void* allocate(size_t) = 0;
     virtual void draw_pixel(int x, int y, int color) = 0;
+    virtual int fs_open(const char* path, int flags) = 0;
+    virtual int fs_read(int fd, char* buf, int len) = 0;
+    virtual int fs_write(int fd, const char* buf, int len) = 0;
+    virtual int fs_close(int fd) = 0;
+    virtual void set_storage(const char* mode) = 0;
 };
