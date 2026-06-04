19a0a7fefbf62ff73670011481bdc3f3cb71d1c3
IFKabir <ifk.ishanfalak@gmail.com>
2026-06-04 11:35:18 +0530

    add read_key, allocate, draw_pixel to hal interface

--- interfaces/hal.h
+++ interfaces/hal.h
@@ -1,8 +1,13 @@
 #pragma once
+#include <cstddef>
+
 class HAL {
 public:
     virtual ~HAL() = default;
     virtual void print_text(const char*) = 0;
     virtual void network_request(const char*) = 0;
     virtual void set_prompt(const char*) = 0;
+    virtual int read_key() = 0;
+    virtual void* allocate(size_t) = 0;
+    virtual void draw_pixel(int x, int y, int color) = 0;
 };
