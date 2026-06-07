d13950484d28bdf29f3ffccb65d310ddd772df2e
IFKabir <ifk.ishanfalak@gmail.com>
2026-06-07 12:58:35 +0530

    refactor: remove shell command implementation and unused HAL interface methods

--- interfaces/hal.h
+++ interfaces/hal.h
@@ -1,17 +1,9 @@
 #pragma once
-#include <cstddef>
 
 class HAL {
 public:
     virtual ~HAL() = default;
     virtual void print_text(const char* text) = 0;
-    virtual void network_request(const char* json) = 0;
     virtual void set_prompt(const char* prompt) = 0;
-    virtual int read_key() = 0;
-    virtual void* allocate(size_t bytes) = 0;
-    virtual void draw_pixel(int x, int y, int color) = 0;
-    virtual int fs_open(const char* path, int flags) = 0;
-    virtual int fs_read(int fd, char* buf, int len) = 0;
-    virtual int fs_write(int fd, const char* buf, int len) = 0;
-    virtual int fs_close(int fd) = 0;
+    virtual void network_request(const char* json) = 0;
 };
--- kernel/hal_wasm.cpp
+++ kernel/hal_wasm.cpp
@@ -1,29 +1,15 @@
 #include "../interfaces/hal.h"
-#include <cstdlib>
 
 extern "C" {
     void js_print_text(const char*);
-    void js_network_request(const char*);
     void js_set_prompt(const char*);
-    int  js_read_key();
-    void js_draw_pixel(int x, int y, int color);
-    int  js_fs_open(const char* path, int flags);
-    int  js_fs_read(int fd, char* buf, int len);
-    int  js_fs_write(int fd, const char* buf, int len);
-    int  js_fs_close(int fd);
+    void js_network_request(const char*);
 }
 
 struct WasmHAL : HAL {
-    void  print_text(const char* t)       override { js_print_text(t); }
-    void  network_request(const char* p)  override { js_network_request(p); }
-    void  set_prompt(const char* p)       override { js_set_prompt(p); }
-    int   read_key()                      override { return js_read_key(); }
-    void* allocate(size_t n)              override { return malloc(n); }
-    void  draw_pixel(int x, int y, int c) override { js_draw_pixel(x, y, c); }
-    int   fs_open(const char* p, int f)   override { return js_fs_open(p, f); }
-    int   fs_read(int fd, char* b, int l) override { return js_fs_read(fd, b, l); }
-    int   fs_write(int fd, const char* b, int l) override { return js_fs_write(fd, b, l); }
-    int   fs_close(int fd)                override { return js_fs_close(fd); }
+    void print_text(const char* t)      override { js_print_text(t); }
+    void set_prompt(const char* p)      override { js_set_prompt(p); }
+    void network_request(const char* p) override { js_network_request(p); }
 };
 
 static WasmHAL g_hal;
